f94adf8d06c4e10e554d528723f7ab06900e1e2d
Victor <suarez.garcia.victor@gmail.com>
2015-06-18 17:51:45 +0200

    cambiando configuracion mysql

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -2,8 +2,8 @@
 # Database: scs_reservas
 # scs@localhost
 
-CREATE DATABASE IF NOT EXISTS scs_reservas;
-USE scs_reservas;
+CREATE DATABASE IF NOT EXISTS tomcatreservas;
+USE tomcatreservas;
 
 #
 # Table structure for table 'HOTEL'
