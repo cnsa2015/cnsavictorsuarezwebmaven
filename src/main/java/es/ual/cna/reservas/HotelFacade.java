14d26c3f9a9c93288fb02ef7b4ae63e806928c19
Victor <suarez.garcia.victor@gmail.com>
2015-06-18 17:20:19 +0200

    commit inicial

---
+++ src/main/java/es/ual/cna/reservas/HotelFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.ual.cna.reservas;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author victor
+ */
+@Stateless
+public class HotelFacade extends AbstractFacade<Hotel> {
+    @PersistenceContext(unitName = "es.ual.cna_reservas_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public HotelFacade() {
+        super(Hotel.class);
+    }
+    
+}
